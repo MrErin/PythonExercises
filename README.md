bf0f782787fdb0172a63856f9558f00de0d9cf4b
MrErin <erinmeaker@gmail.com>
2018-07-06 15:59:24 -0500

    organization

--- README.md
+++ README.md
@@ -16,3 +16,11 @@
   * [Zoo]('./Regular/zoo.py)
 * Comprehensions
   * [Family Dictionary](./Regular/family_dict.py)
+  * [Flowers](./Orientation/flowers.py)
+* Functions
+  * [Lightning-01](./Lightning/lightning-01.py)
+  * [Human Sizes](./Orientation/humansizes.py)
+* Importing
+  * [Human Sizes](./Orientation/humansizes.py) & [Human Sizes Test](./Orientation/humansizesTest.py)
+* Classes
+  * [Zoo (Orientation)](./Orientation/zoo.py)
--- Regular/stocks.py
+++ Regular/stocks.py
@@ -13,8 +13,12 @@ purchases = [
 
 
 def purchase_history():
-    for purchase in purchases
-        print(f'')
+    for purchase in purchases:
+        print(
+            f'I purchased {purchase[1]} of {stockDict[ticker]} at {purchase[3]}; a total value of {purchase[1] * purchase[3]}'
+            for (ticker, company) in stockDict.items() if
+
+        )
 
 
 # Create a second purchase summary that which accumulates total investment by ticker symbol. In the above sample data, there are two blocks of GE. These can easily be combined by creating a dict where the key is the ticker and the value is the list of blocks purchased. The program makes one pass through the data to create the dict. A pass through the dict can then create a report showing each ticker symbol and all blocks of stock.
