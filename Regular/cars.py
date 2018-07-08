414a4ceea9e71f71d223733e2152af7661e03fc4
MrErin <erinmeaker@gmail.com>
2018-07-08 10:39:29 -0500

    refining exercises

--- Regular/cars.py
+++ Regular/cars.py
@@ -1,15 +1,24 @@
+# Create an empty set named `showroom`.
 showroom = set()
 print(len(showroom))
 print(f'When it\'s empty the showroom contains: {showroom}')
+
+# Add four of your favorite car model names to the set.
+# Print the length of your set.
 showroom.add('Nimbus 2000')
 showroom.add('Cleansweep Five')
 showroom.add('Firebolt')
 showroom.add('Nimbus 2001')
 print(len(showroom))
 print(f'Now the showroom contains: {showroom}')
+
+# Pick one of the items in your show room and add it to the set again.
+# Print your showroom. Notice how there's still only one instance of that model in there.
 showroom.add('Comet 140')
 print(len(showroom))
 print(f'Now the showroom contains: {showroom}')
+
+# Using `update()`, add two more car models to your showroom with another set.
 new_models = set()
 new_models.add('Silver Arrow')
 new_models.add('Air Wave Gold')
@@ -18,8 +27,12 @@ print(f'Yay! New Models! {new_models}')
 showroom.update(new_models)
 print(len(showroom))
 print(f'Now the showroom contains: {showroom}')
+
+# You've sold one of your cars. Remove it from the set with the `discard()` method.
 showroom.discard('Comet 140')
 print(f'after discard, the showroom contains: {showroom}')
+
+# Now create another set of cars in a variable `junkyard`. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the `showroom` set.
 junkyard = set()
 junkyard.add('Comet 140')
 junkyard.add('Silver Arrow')
@@ -30,8 +43,23 @@ junkyard.add('Nimbus 1000')
 junkyard.add('Nimbus 2000')
 print(len(junkyard))
 print(f'Junkyard contains: {junkyard}')
+print(f'Showroom contains: {showroom}')
+
+# Use the `intersection` method to see which cars exist in both the showroom and that junkyard.
 print(
     f'These brooms are in both the junkyard and the showroom: {set.intersection(junkyard, showroom)}')
+
+# Now you're ready to buy the cars in the junkyard. Use the `union` method to combine the junkyard into your showroom.
 print(
-    f'these are the brooms in the showroom after the junkyard purchase: {showroom.union(junkyard)}')
-junkyard.discard()
+    f'These are the brooms in the showroom after the junkyard purchase: {showroom.union(junkyard)}')
+
+# Use the `discard()` method to remove any cars that you acquired from the junkyard that you want in your showroom.
+junkyard.discard('Twigger 90')
+junkyard.discard('Moontrimmer')
+junkyard.discard('Cleansweep Eleven')
+junkyard.discard('Nimbus 1000')
+print(f'Now the junkyard still has: {junkyard}')
+print(f'The showroom contains: {showroom}')
+
+#  ? The Comet 140 is the one discarded from the showroom. There was one in the junkyard, and it should have been added to the showroom during the purchase, but it wasn't It just disappears. Why?
+print(f'WHAT HAPPENED TO THE COMET 140???')
--- Regular/planets.py
+++ Regular/planets.py
@@ -1,16 +1,47 @@
+# echo 'planet_list = ["Mercury", "Mars"]' >> planets.py
 planet_list = ["Mercury", "Mars"]
+
+# Use `append()` to add Jupiter and Saturn at the end of the list.
 planet_list.append("Jupiter")
 planet_list.append("Saturn")
 print(planet_list)
+
+# Use the `extend()` method to add another list of the last two planets in our solar system to the end of the list.
 planet_list.extend(['Uranus', 'Neptune'])
 print(planet_list)
+
+# Use `insert()` to add Earth, and Venus in the correct order.
 planet_list.insert(2, 'Earth')
 planet_list.insert(3, 'Venus')
 print(planet_list)
+
+# Use `append()` again to add Pluto to the end of the list.
 planet_list.append('Pluto')
 print(planet_list)
+
+# Now that all the planets are in the list, slice the list in order to get the rocky planets into a new list called `rocky_planets`.
 rocky_planets = planet_list[4:]
 print(rocky_planets)
 print(len(planet_list))
+
+# Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the `del` operation to remove it from the end of `planet_list`.
 del planet_list[8]
 print(planet_list)
+
+# Create another list containing tuples. Each tuple will hold the name of a spacecraft that we have launched, and the names of the planet(s) that it has visited, or landed on. (e.g. `('Cassini', 'Saturn')`).
+spacecraft = [('Saturn', 'Cassini'), ('Jupiter', 'Gallileo'),
+              ('Mars', 'Curiosity'), ('Saturn', 'Fictional Spacecraft')]
+
+# Iterate over your list of planets, and inside that loop, iterate over the list of tuples. Print, for each planet, which sattelites have visited.
+
+
+def missions():
+    for planet in planet_list:
+        for craft in spacecraft:
+            if planet in craft[0]:
+                print(
+                    f'Planet: {planet}; Spacecraft: {craft[1]}'
+                )
+
+
+missions()
